c8c9c886645e5c6980c431630db254dc74c2a20d
Ritika Bansal <ritikabansal5678@gmail.com>
2020-09-14 13:48:33 +0530

    first commit

---
+++ Day-02/tut2.js
@@ -0,0 +1,23 @@
+console.log('JavaScript_Learning_Day-02');
+
+// Types of Console
+
+console.log('Hello Console');
+console.log('hello');
+console.log(4*5);
+console.log(true);
+console.log([34,2,1,2])
+console.log({harry:"this",marks:80});
+console.table({harry:"this",marks:80});
+console.warn("This is a warning");
+//console.clear();
+console.time('Your code Took');
+// your code
+console.timeEnd('Your code Took');
+console.assert(566<189,'Age>189 is not possible');
+console.error('This is an error');
+/*
+this
+is a
+multiline comment
+*/
