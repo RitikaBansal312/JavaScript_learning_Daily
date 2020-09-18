9f70abb5437820cbfd2ca89a8ea396e954522677
Ritika Bansal <ritikabansal5678@gmail.com>
2020-09-18 11:54:18 +0530

    first commit

---
+++ Day-12/tut12.js
@@ -0,0 +1,28 @@
+console.log("JavaScript_Learning_Day-12");
+
+// Understanding DOM
+
+let a = document;
+a = document.all;  // prints HTML ALL Collection
+console.log(a);
+a = document.body;   // prints HTML BODY Collection
+console.log(a);
+a = document.forms[0];   // prints first form
+Array.from(a).forEach(function(element){
+    console.log(element);
+})
+a = document.links[0];  // to extract the link
+console.log(a);
+
+/* use document.images and document.
+scripts and print the list of images 
+and scripts on an html page
+*/
+
+a = document.images;  // print the list of images
+console.log(a);
+a = document.scripts;   // print the list of scripts
+console.log(a);
+a = document.links;    // print the link 
+console.log(a);
+
