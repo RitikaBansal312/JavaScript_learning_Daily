7f8189075a816a480765527ffd3e538bca0d7664
Ritika Bansal <ritikabansal5678@gmail.com>
2020-09-29 14:02:40 +0530

    first commit

---
+++ Day-22/tut22.js
@@ -0,0 +1,46 @@
+console.log('JavaScript_Learning_Day-22');
+
+// Math Object In JavaScript
+
+let x = 4;
+let y = 5;
+let z;
+console.log(z);
+z = Math;
+console.log(z);
+z = Math.PI;
+console.log(z);
+z = Math.E;
+console.log(z);
+z = Math.round(5.4);
+console.log(z);
+z = Math.round(5.3);
+console.log(z);
+z = Math.ceil(5.3);
+console.log(z);
+z = Math.floor(-5.6);
+console.log(z);
+z = Math.floor(5.6);
+console.log(z);
+z = Math.abs(5);
+console.log(z);
+z = Math.abs(-5.6);
+console.log(z);
+z = Math.abs(5.6);
+console.log(z);
+z = Math.sqrt(64);
+console.log(z);
+z = Math.pow(2, 3);
+console.log(z);
+z = Math.min(2, 3, 34,234, 2342,34, );
+console.log(z);
+z = Math.max(2, 3, 34,234,34);
+console.log(z);
+z = Math.random();
+console.log(z);
+z = 100 * Math.random();
+console.log(z);
+z = (50 + (100 - 50) * Math.random());
+console.log(z);
+z = Math.ceil(50 + (100-50)*Math.random());
+console.log(z);
