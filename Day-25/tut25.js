234e6442e1887ebc51b7329439113bc9c9b5d033
Ritika Bansal <ritikabansal5678@gmail.com>
2020-10-01 12:31:03 +0530

    first commit

---
+++ Day-25/tut25.js
@@ -0,0 +1,27 @@
+console.log("JavaScript_Learning_Day-23");
+
+// Object Prototype In javascript
+
+// Object literal : Object.prototype
+let obj = {
+    name: "ritika",
+    channel: "abcd",
+    address: "sre"
+}
+console.log(obj);
+
+function Obj(givenName){
+    this.name = givenName
+}
+
+Obj.prototype.getName = function (){
+    return this.name;
+}
+
+Obj.prototype.setName = function (newName){
+  this.name = newName;
+}
+
+let obj2 = new Obj("rashi");
+console.log(obj2);
+ 
