6c7e78711f56503df7eaea3e492a20ea5c0b335a
Ritika Bansal <ritikabansal5678@gmail.com>
2020-10-12 03:02:38 +0530

    first commit

--- Day-21_Project/Notes_App/js/app.js
+++ Day-21_Project/Notes_App/js/app.js
@@ -1,5 +1,6 @@
 console.log("Welcome to notes app. This is app.js");
 showNotes();
+
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
@@ -17,6 +18,7 @@ addBtn.addEventListener("click", function (e) {
   showNotes();
 })
 
+// Function to show elements from localStorage
 function showNotes() {
   let notes = localStorage.getItem("notes");
   if (notes == null) {
@@ -31,7 +33,7 @@ function showNotes() {
                 <div class="card-body">
                     <h5 class="card-title">Note ${index + 1}</h5>
                     <p class="card-text">${element}</p>
-                    <button href="#" class="btn btn-primary">Delete Note</button>
+                    <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
          </div>
          `;
@@ -43,3 +45,42 @@ function showNotes() {
     notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
   }
 }
+
+// Function to delete a note
+function deleteNote(index) {
+  // console.log("I am deleting", index);
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function () {
+
+  let inputVal = search.value.toLowerCase();
+  // console.log('Input event fired!', inputVal);
+  let noteCards = document.getElementsByClassName('notecard');
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerText;
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    }
+    else {
+      element.style.display = "none";
+    }
+    // console.log(cardTxt);
+  })
+})
+/*
+Further Features:
+1. Add Title
+2. Mark a note as Important
+3. Separate notes by user
+4. Sync and host to web server
+*/
