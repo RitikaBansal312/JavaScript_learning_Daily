38b8674ddbeebccbed79e0508b2ef9a8512f5f2a
Ritika Bansal <ritikabansal5678@gmail.com>
2020-10-12 01:19:24 +0530

    first commit

--- Day-21_Project/Notes_App/js/app.js
+++ Day-21_Project/Notes_App/js/app.js
@@ -1,8 +1,8 @@
 console.log("Welcome to notes app. This is app.js");
-
+showNotes();
 // If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
-addBtn.addEventListener("click", function(e) {
+addBtn.addEventListener("click", function (e) {
   let addTxt = document.getElementById("addTxt");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
@@ -10,4 +10,36 @@ addBtn.addEventListener("click", function(e) {
   } else {
     notesObj = JSON.parse(notes);
   }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  console.log(notesObj);
+  showNotes();
 })
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach(function (element, index) {
+    html += `
+         <div class="notecard my-2 mx-2 card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Note ${index + 1}</h5>
+                    <p class="card-text">${element}</p>
+                    <button href="#" class="btn btn-primary">Delete Note</button>
+                </div>
+         </div>
+         `;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+  }
+}
